5b54f7c34242b86bace9a70baf6b3ec54d287cf4
ZhilPro27 <zhilalkrisna@gmail.com>
2024-04-26 15:05:24 +0700

    Menambahkan algoritma untuk insert node

--- LinkList.cpp
+++ LinkList.cpp
@@ -19,4 +19,10 @@ void addNode() {
 	cin >> nama;
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
 }
