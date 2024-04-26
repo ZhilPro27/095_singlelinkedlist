9a5e472bb5ee1bbd91acc63333c928f1595af404
ZhilPro27 <zhilalkrisna@gmail.com>
2024-04-26 15:01:13 +0700

    algoritma untuk menerima data dari user di dalam prosedur addNode

--- LinkList.cpp
+++ LinkList.cpp
@@ -12,4 +12,11 @@ Node* START = NULL;
 void addNode() {
 	int nim;
 	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
 }
